ec0b590b7994dc357eae5bb7145588891c394e10
Christoffer T. Timm <kontakt@christoffertimm.de>
2023-05-31 19:39:16 +0200

    Initial Xcode Project

---
+++ Open Offload/Open Offload/Open_OffloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_OffloadApp.swift
+//  Open Offload
+//
+//  Created by Christoffer T. Timm on 31.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Open_OffloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
